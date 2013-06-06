88528ec9db21b5780b6f3d53bfd6207b111d4a31
Diego Figueroa <diegofigueroa99@gmail.com>
2013-06-06 07:21:11 +0600

    minor UI changes, new footer and header colors.

--- assets/www/js/defaults.js
+++ assets/www/js/defaults.js
@@ -9,9 +9,9 @@ $(document).bind('mobileinit', function(){
 	$.mobile.loader.prototype.options.html = "";
 	
 	// Theme
-	$.mobile.page.prototype.options.theme  = 'b';
-	//$.mobile.page.prototype.options.headerTheme = 'b';
-	//$.mobile.page.prototype.options.footerTheme = 'b';
-	$.mobile.page.prototype.options.contentTheme = 'b';
-	$.mobile.page.prototype.options.backBtnTheme = 'b';
+	$.mobile.page.prototype.options.theme  = 'a';
+	$.mobile.page.prototype.options.headerTheme = 'a';
+	$.mobile.page.prototype.options.footerTheme = 'a';
+	$.mobile.page.prototype.options.contentTheme = 'a';
+	$.mobile.page.prototype.options.backBtnTheme = 'a';
 });
